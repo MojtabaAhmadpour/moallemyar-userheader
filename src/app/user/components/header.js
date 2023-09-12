4436ebd0a909ebcc62c05035d781e1936e904c43
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-09-12 17:28:29 +0330

    change style of ltr to rtl

--- src/app/user/components/header.js
+++ src/app/user/components/header.js
@@ -1,5 +1,5 @@
 import style from "./header.module.css";
-import { FaAlignLeft } from "react-icons/fa";
+import { RiMenu4Line } from "react-icons/Ri";
 import { FaComments } from "react-icons/fa";
 import { BsBellFill } from "react-icons/Bs";
 import Image from "next/image";
@@ -10,24 +10,6 @@ const UserHeader = () => {
   return (
     <div className={style.UserHeader}>
       <div className="flex">
-        <div className="">
-          <FaAlignLeft />
-        </div>
-        <strong>Search Jobs</strong>
-        <div className="">search bar</div>
-        <div className="">
-          <FaComments />
-        </div>
-        <div className="">
-          <BsBellFill />
-        </div>
-        <div className="">
-          <Image
-            src={userimage}
-            width={40}
-            style={{ borderRadius: 200, padding: -10 }}
-          />
-        </div>
         <div className="flex flex-col">
           <div className="">
             <p>username</p>
@@ -36,24 +18,45 @@ const UserHeader = () => {
             <p>underusername title</p>
           </div>
         </div>
+        <div className="">
+          <Image
+            src={userimage}
+            width={40}
+            style={{ borderRadius: 200, padding: -10 }}
+          />
+        </div>
+        <div className="">
+          <BsBellFill />
+        </div>
+        <div className="">
+          <FaComments />
+        </div>
+        <div className="">search bar</div>
+        <strong>Search Jobs</strong>
+        <div className="">
+          <RiMenu4Line />
+        </div>
       </div>
       <div className="">
-        <div className="flex bg-white mx-4 rounded-md">
-          <div className="">dropdown filter</div>
-          <div className="">search bar</div>
-          <div className="">
-            <button className="bg-indigo-50">text</button>
-            <button className=" bg-indigo-600"> text<BiSearchAlt/></button>
+        <div className="flex bg-white mx-4 rounded-md p-2 ">
+          <div className=" mx-2">
+            <button className="bg-indigo-50 mx-2">text</button>
+            <button className=" bg-indigo-600 mx-2">
+              text
+              <BiSearchAlt />
+            </button>
           </div>
+          <div className=" mx-2">search bar</div>
+          <div className=" mx-2">dropdown filter</div>
         </div>
       </div>
       <div className="flex">
+        <button className="bg-indigo-300 mx-1">text</button>
+        <button className="bg-indigo-300 mx-1">text</button>
+        <button className="bg-indigo-300 mx-1">text</button>
+        <button className="bg-indigo-300 mx-1">text</button>
+        <button className="bg-indigo-300 mx-1">text</button>
         <p>text</p>
-        <button className="bg-indigo-300 mx-1">text</button>
-        <button className="bg-indigo-300 mx-1">text</button>
-        <button className="bg-indigo-300 mx-1">text</button>
-        <button className="bg-indigo-300 mx-1">text</button>
-        <button className="bg-indigo-300 mx-1">text</button>
       </div>
     </div>
   );

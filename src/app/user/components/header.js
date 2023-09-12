c1eb0a3a7bcf487b37b83b7152b5a065e3689174
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-09-13 03:27:27 +0330

    complete design row 1

--- src/app/user/components/header.js
+++ src/app/user/components/header.js
@@ -5,36 +5,41 @@ import { BsBellFill } from "react-icons/Bs";
 import Image from "next/image";
 import userimage from "../../../../public/assets/img/userimage.jpg";
 import { BiSearchAlt } from "react-icons/Bi";
+import Link from "next/link";
 
 const UserHeader = () => {
   return (
     <div className={style.UserHeader}>
-      <div className="flex">
-        <div className="flex flex-col">
-          <div className="">
-            <p>username</p>
+      <div className="flex justify-items-center items-center justify-around ">
+        <div className="flex  items-center">
+          <div className="flex flex-col m-2">
+            <p className="text-right font-bold" text-md>
+              name
+            </p>
+            <p className="text-right font-light text-xs">username</p>
           </div>
-          <div className="">
-            <p>underusername title</p>
-          </div>
+          <Image src={userimage} className="w-10 h-10 rounded-3xl m-2" />
+          <BsBellFill className="mx-3 text-4xl bg-white rounded-3xl p-2" />
+          <FaComments className="mx-3 text-4xl bg-white rounded-3xl p-2" />
         </div>
-        <div className="">
-          <Image
-            src={userimage}
-            width={40}
-            style={{ borderRadius: 200, padding: -10 }}
-          />
+        <div className=" ">
+          <form className="">
+            <button type="submit">
+              <BiSearchAlt className=" text-indigo-400 relative left-7 top-1 text-lg" />
+            </button>
+            <input
+              type="text"
+              placeholder="جستجو"
+              className="w-96 bg-indigo-100 rounded-3xl p-2 pr-4 text-right text-indigo-400 "
+            />
+          </form>
         </div>
-        <div className="">
-          <BsBellFill />
-        </div>
-        <div className="">
-          <FaComments />
-        </div>
-        <div className="">search bar</div>
-        <strong>Search Jobs</strong>
-        <div className="">
-          <RiMenu4Line />
+
+        <div className="flex  items-center ">
+          <strong className="mr-6">Search Jobs</strong>
+          <Link href="/">
+            <RiMenu4Line className="text-4xl" />
+          </Link>
         </div>
       </div>
       <div className="">
